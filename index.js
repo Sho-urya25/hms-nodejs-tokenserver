d5e1f497caa3b5cb2b6e4d5003c45a5f1ad1c13b
Sho-urya25 <121052001+Sho-urya25@users.noreply.github.com>
2023-01-29 12:28:39 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,78 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const uuid4 = require('uuid4');
+const cors = require('cors');
+const dotenv = require('dotenv');
+dotenv.config();
+const app = express();
+const app_secret = process.env.APP_SECRET;
+const app_access_key = process.env.APP_ACCESS_KEY;
+const port = process.env.PORT || 8000
+
+console.log(app_access_key,app_secret);
+
+const generateAuthToken = (req,resp)=>{
+    var payload = {
+        access_key: app_access_key,
+        room_id: req.params.roomid,
+        user_id: req.params.userId,
+        role: req.params.role,
+        type: 'app',
+        version: 2,
+        iat: Math.floor(Date.now() / 1000),
+        nbf: Math.floor(Date.now() / 1000)
+    };
+    
+    if(req.params.roomid!= null &&req.params.userId!= null&&req.params.role!= null){
+        jwt.sign(
+            payload,
+            app_secret,
+            {
+                algorithm: 'HS256',
+                expiresIn: '24h',
+                jwtid: uuid4()
+            },
+            function (err, token) {
+               resp.send({"token":token});
+            }
+        );
+    }else{
+        resp.send("somefiels are not field");
+    }
+    
+}
+const generateManagementToken = (req,resp)=>{
+    var payload = {
+        access_key: app_access_key,
+        type: req.params.type,
+        version: 2,
+        iat: Math.floor(Date.now() / 1000),
+        nbf: Math.floor(Date.now() / 1000)
+    };
+    
+    if(req.params.type!= null){
+        jwt.sign(
+            payload,
+            app_secret,
+            {
+                algorithm: 'HS256',
+                expiresIn: '24h',
+                jwtid: uuid4()
+            },
+            function (err, token) {
+               resp.send({"token":token});
+            }
+        );
+    }else{
+        resp.send("somefiels are not field");
+    }
+    
+}
+
+app.options('*', cors());
+app.get('/hmsatkn/:roomid/:userId/:role/',generateAuthToken);
+app.get('/hmsmtkn/:type/',generateManagementToken);
+
+app.listen(port,()=>{
+    console.log(port);
+});
